8ba36313e1a8546f947cbe1a51b4aa77f3590adc
Pablo Ricco <pricco@gmail.com>
2013-10-06 22:08:59 -0200

    added color

--- configure.sh
+++ configure.sh
@@ -1,4 +1,5 @@
-mkdir -p ~/.vim/autoload ~/.vim/bundle
+mkdir -p ~/.vim/autoload ~/.vim/bundle ~/.vim/colors
 curl -Sso ~/.vim/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+curl -Sso ~/.vim/colors/monokai.vim https://raw.github.com/sickill/vim-monokai/master/colors/Monokai.vim
 git submodule init
 cp .vimrc ~/.vimrc
--- vimrc
+++ vimrc
@@ -1,5 +1,6 @@
 execute pathogen#infect()
 syntax on
 filetype plugin indent on
+colorscheme monokai
 autocmd vimenter * if !argc() | NERDTree | endif
 autocmd bufenter * if (winnr("$") == 1 && exists("b:NERDTreeType") && b:NERDTreeType == "primary") | q | endif
