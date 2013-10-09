86e49e1829ebea5534cb69ca5c7fbdb81646b4ba
Pablo Ricco <pricco@gmail.com>
2013-10-09 15:18:22 -0200

    change terminal colors

--- terminal.sh
+++ terminal.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+#wget https://github.com/todylu/monaco.ttf/raw/master/monaco.ttf
 gnome_term_profile=/apps/gnome-terminal/profiles/Default
 
 palette="#1B1B1D1D1E1E:#F9F926267272:#8282B4B41414:#FDFD97971F1F:#5656C2C2D6D6:#8C8C5454FEFE:#464654545757:#CCCCCCCCC6C6:#505053535454:#FFFF59599595:#B6B6E3E35454:#FEFEEDED6C6C:#8C8CEDEDFFFF:#9E9E6F6FFEFE:#89899C9CA1A1:#F8F8F8F8F2F2"
 bd_color="#000000000000"
-fg_color="#A0A0A0A0A0A0"
-bg_color="#1C1C1C1C1C1C"
+fg_color="#FFFFFFFFFFFF"
+bg_color="#262626262626"
 
-gconftool-2 -s -t string $gnome_term_profile/font "Ubuntu Mono 10"
+gconftool-2 -s -t string $gnome_term_profile/font "Monaco 9"
 
 gconftool-2 -s -t string $gnome_term_profile/palette $palette
 
