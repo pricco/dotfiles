be9734a06317155f1fd87502b7cd1a0b32a52583
Pablo Ricco <pricco@gmail.com>
2013-10-16 03:52:13 -0200

    update vimrc

--- configure.sh
+++ configure.sh
@@ -1,6 +1,6 @@
 mkdir -p ~/.vim/autoload ~/.vim/bundle ~/.vim/colors
-curl -Sso ~/.vim/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
-curl -Sso ~/.vim/colors/monokai.vim https://raw.github.com/sickill/vim-monokai/master/colors/Monokai.vim
+curl -Sso ~/.vim/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/HEAD/autoload/pathogen.vim 
+curl -Sso ~/.vim/colors/Monokai.vim https://raw.github.com/sickill/vim-monokai/master/colors/Monokai.vim
 git submodule init
 ln -s  $HOME/.vim/.vimrc $HOME/.vimrc
 
