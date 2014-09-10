bd1ce5ff08a81e9187e0b36dc72e30e705a8f937
Pablo Ricco <pricco@gmail.com>
2014-09-10 18:19:58 -0300

    Remove pathogen.

--- configure.sh
+++ configure.sh
@@ -1,5 +1,4 @@
 mkdir -p ~/.vim/autoload ~/.vim/bundle ~/.vim/colors
-curl -Sso ~/.vim/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/HEAD/autoload/pathogen.vim 
 curl -Sso ~/.vim/colors/monokai.vim https://raw.githubusercontent.com/sickill/vim-monokai/master/colors/monokai.vim
 git submodule init
 ln -s  $HOME/.vim/.vimrc $HOME/.vimrc
