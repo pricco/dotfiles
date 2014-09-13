07bfa6a52af84790756e63c15df478763edc4de3
pricco <pricco@gmail.com>
2014-09-13 02:17:20 -0300

    Fix submodules relative path

--- configure.sh
+++ configure.sh
@@ -1,6 +1,7 @@
 mkdir -p ~/.vim/autoload ~/.vim/bundle ~/.vim/colors
 curl -Sso ~/.vim/colors/monokai.vim https://raw.githubusercontent.com/sickill/vim-monokai/master/colors/monokai.vim
 git submodule init
+git submodule update
 ln -s  $HOME/.vim/.vimrc $HOME/.vimrc
 
 #python
