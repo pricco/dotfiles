45b426fc5fb2e2486053818010203ec2969d9ce2
Pablo Ricco <pricco@gmail.com>
2025-05-16 02:36:06 -0300

    Set catppucin as theme.

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -5,17 +5,17 @@ return {
       signs = {
         add = { text = '+' },
         change = { text = '' },
-        delete = { text = '󱘹' },
-        topdelete = { text = '󱘹' },
-        changedelete = { text = '󰦒' },
+        delete = { text = '−' },
+        topdelete = { text = '−' },
+        changedelete = { text = '±' },
         untracked = { text = '' },
       },
       signs_staged = {
         add = { text = '󱇬' },
         change = { text = '' },
-        delete = { text = '󱘹' },
-        topdelete = { text = '󱘹' },
-        changedelete = { text = '󰦒' },
+        delete = { text = '−' },
+        topdelete = { text = '−' },
+        changedelete = { text = '±' },
         untracked = { text = '' },
       },
     })
--- nvim/lua/plugins/ufo.lua
+++ nvim/lua/plugins/ufo.lua
@@ -13,6 +13,7 @@ return {
     })
 
     vim.o.foldcolumn = 'auto:9'
+    vim.o.foldcolumn = '0'
     vim.o.foldlevel = 99
     vim.o.foldlevelstart = 99
     vim.o.foldenable = true
